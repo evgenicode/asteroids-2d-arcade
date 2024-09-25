ae836cc0414b05a14bbdf2814619bf90495efed7
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-09-25 17:14:53 +0300

    add screen wrap

--- player/player.gd
+++ player/player.gd
@@ -4,13 +4,21 @@ extends RigidBody2D
 @export var spin_power = 8000
 var thrust = Vector2.ZERO
 var rotation_dir = 0
+var screensize = Vector2.ZERO
 
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
 
 func _ready():
 	change_state(ALIVE)
+	screensize = get_viewport_rect().size
 	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+
 func change_state(new_state):
 	match new_state:
 		INIT: 
